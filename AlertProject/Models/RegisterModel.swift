634ad95bede5c21c49be55b4aaf89ad719dd8743
Mesut Öztürk <mesutoztrk@msn.com>
2019-03-28 21:38:40 +0300

    validator register

---
+++ AlertProject/Models/RegisterModel.swift
@@ -0,0 +1,54 @@
+//
+//  RegisterModel.swift
+//  AlertProject
+//
+//  Created by Mesut Öztürk on 28.03.2019.
+//  Copyright © 2019 Mesut Öztürk. All rights reserved.
+//
+
+import UIKit
+
+class RegisterModel: NSObject {
+    var UserName = ""
+    var Password = ""
+    var ConfirmPassword = ""
+    
+    var isValid = false
+    
+    func validate() -> (Bool, String) {
+        var message = "";
+        var isValid = true
+        if self.UserName.count == 0 {
+            message += "Kullanıcı adı boş geçilemez \n"
+            isValid = false
+        }
+        else if checkRange(body: UserName, min: 3, max: 16) {
+            message += "Kullanıcı adı 3 ile 16 karakter arasında olmalıdır \n"
+            isValid = false
+        }
+        if self.Password.count == 0 {
+            message += "Şifreyi boş bırakamazsınız \n"
+            isValid = false
+        }
+        else if(checkRange(body: Password, min: 6, max: 18)){
+            message += "Şifre 6-18 karakter aralığında olmalıdır \n"
+            isValid = false
+        }
+        if self.ConfirmPassword.count == 0 {
+            message += "Şifrenizi onaylayınız \n"
+            isValid = false
+        }
+        else if self.Password != self.ConfirmPassword{
+            message += "Şifreleriniz uyuşmuyor"
+            isValid = false
+        }
+        
+        
+        return (isValid,message)
+    }
+    
+   private func checkRange(body: String, min: Int, max: Int) -> Bool {
+        let result = body.count < min || body.count > max
+        return result
+    }
+}
--- AlertProject/ViewController.swift
+++ AlertProject/ViewController.swift
@@ -9,12 +9,51 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var txtUsername: UITextField!
+    @IBOutlet weak var txtPassword: UITextField!
+    @IBOutlet weak var txtConfirmPassword: UITextField!
+    var registerModel = RegisterModel()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func btnAction(_ sender: Any) {
+        /*
+        let alert = UIAlertController(title: "Error", message: "Kullanıcı adı hatalı", preferredStyle: .alert)
+        
+        let okButton = UIAlertAction(title: "OK", style: .default, handler: nil)
+        
+        alert.addAction(okButton)
+        
+        self.present(alert, animated: true, completion: nil)
+        */
+        registerModel = RegisterModel()
+        registerModel.UserName = txtUsername.text!
+        registerModel.Password = txtPassword.text!
+        registerModel.ConfirmPassword = txtConfirmPassword.text!
+        
+        var result = registerModel.validate()
+        if !result.0 {
+            let alert = UIAlertController(title: "Error", message: result.1, preferredStyle: .alert)
+            
+            let okButton = UIAlertAction(title: "Ok", style: .default, handler: nil)
+            
+            alert.addAction(okButton)
+            
+            self.present(alert, animated: true, completion: nil)
+        }else {
+            let alert = UIAlertController(title: "Kayıt", message: "Kayıt işlemi başarılı", preferredStyle: .alert)
+            
+            let okButton = UIAlertAction(title: "Ok", style: .default, handler: nil)
+            
+            alert.addAction(okButton)
+            
+            self.present(alert, animated: true, completion: nil)
+        }
+    }
+    
 }
 
