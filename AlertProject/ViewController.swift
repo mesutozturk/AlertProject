7e3a84fc56f3fa3d32d94a91b818c23ef7fb6e9d
Mesut Öztürk <mesutoztrk@msn.com>
2019-03-28 20:52:06 +0300

    Initial Commit

---
+++ AlertProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AlertProject
+//
+//  Created by Mesut Öztürk on 28.03.2019.
+//  Copyright © 2019 Mesut Öztürk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
